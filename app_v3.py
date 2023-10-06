179f12e2bf5d05b5b0cbc89cc5a422554b95eaf6
João Augusto Palmeira <palmeira.joao@gmail.com>
2023-10-06 18:33:52 -0300

    atualização do app_v3

--- app_v3.py
+++ app_v3.py
@@ -2,17 +2,46 @@ from abc import ABC, abstractclassmethod, abstractproperty
 from datetime import datetime
 
 
-class Cliente:
-    pass
+class PessoaFisica:
+    def __init__(self, cpf, nome, data_nascimento):
+        self.cpf = cpf
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        
 
-class PessoaFisica(Cliente):
-    pass
+class Cliente(PessoaFisica):
+    def __init__(self, cpf, nome, data_nascimento, endereco):
+        super().__init__(cpf, nome, data_nascimento)
+        self.endereco = endereco
+        self.contas = []
+
+    def realizar_transacao(conta, transacao):
+        pass
+
+    def adicionar_conta(conta):
+        pass
+
 
 class Conta:
-    pass
+    def __init__(self, saldo, numero, agencia, cliente, historico):
+        self.saldo = saldo
+        self.numero = numero
+        self.agencia = agencia
+        self.cliente = cliente
+        self.historico = historico
+
+    def mostrar_saldo(self, saldo):
+        self.saldo = saldo
+
+    def nova_conta(self, cliente, numero, conta):
+        pass
+        
 
 class ContaCorrente(Conta):
-    pass
+    def __init__(self, saldo, numero, agencia, cliente, historico, limite, limite_saques=3):
+        super().__init__(saldo, numero, agencia, cliente, historico)
+        self.limite = limite
+
 
 class Historico:
     pass
